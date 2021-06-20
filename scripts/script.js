78d5532a6ffb21f34083a545a9b460dbf16ca05a
Stan-RUS <stan.suprunoff@gmail.com>
2021-06-20 14:47:44 +0300

    complete final draft

--- scripts/script.js
+++ scripts/script.js
@@ -1,33 +1,37 @@
-const content = document.querySelector('.content');
-const popupClose = content.querySelector('.popup__close');
-const popUp = content.querySelector('.popup');
-const infoTitle = content.querySelector('.profile__info-title');
-const infoSubtitle = content.querySelector('.profile__info-subtitle');
-const popupForm = content.querySelector('.popup__form');
-const editButton = content.querySelector('.profile__edit-button');
+const popupElement = document.querySelector('.popup')
+const popupCloseButtonElement = popupElement.querySelector('.popup__close');
 
-function openPopup() {
-	popUp.classList.add('popup_opened');
+const popupEditButtonElement = document.querySelector('.popup__edit');
+
+const popupAddButtonElement = document.querySelector('.popup__add');
+
+const togglePopupVisibility = function() {
+	popupElement.classList.toggle('popup_is-opened')
 }
 
-function closePopup() {
-	popUp.classList.remove('popup_opened');
+const openPopup = function() {
+	popupElement.classList.add('popup_is-opened')
 }
 
-function formSubmit(e) {
-	e.preventDefault();
-
-	infoTitle.textContent = inputTitle.value;
-	infoSubtitle.textContent = inputSubtitle.value;
-
-	closePopup();
+const closePopup = function() {
+	popupElement.classList.remove('popup_is-opened')
 }
 
-function editProfile() {
-	inputTitle.value = infoTitle.textContent;
-	inputSubtitle.value = infoSubtitle.textContent;
+const closePopupByClickOnOverlay = function(event) {
+	if (event.target != event.currentTarget) {return
+	}
+	closePopup() 
 }
 
-popupClose.addEventListener('click', closePopup);
-popupForm.addEventListener('submit', formSubmit);
-editButton.addEventListener('click', editProfile);
+popupEditButtonElement.addEventListener('click', openPopup)
+
+
+popupCloseButtonElement.addEventListener ('click', closePopup)
+
+popupElement.addEventListener('click', closePopupByClickOnOverlay)
+
+
+
+
+
+
